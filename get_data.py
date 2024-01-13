a155654e824f1a296cb7769816c5838944293598
CuriousKomodo <kefei.hu@transferwise.com>
2024-01-13 12:49:38 +0000

    comments

--- get_data.py
+++ get_data.py
@@ -4,15 +4,17 @@ import requests
 from pprint import pprint
 from authentication import authenticate
 
+"""Fetch all the child ids given the parent incident page, 
+then fetch the content from each child page"""
+
 api_token = os.getenv('API_TOKEN')
 email = os.getenv('EMAIL')
 headers = authenticate(email, api_token)
 
 base_url = 'https://transferwise.atlassian.net/wiki'
 page_id = "2740585401"  # This is the incident parent page for 2023
-url = f'{base_url}/rest/api/content/{page_id}/child/page'
 
-def get_child_ids_from_response(child_page_results):
+def _get_child_ids_from_response(child_page_results):
     child_ids = []
     for page in child_page_results:
         link = page['_links'].get('self')
@@ -34,7 +36,7 @@ def get_ids_of_all_child_page_ids(page_id):
         )
         if response.status_code == 200:
             child_page_results = response.json()['results']
-            child_ids = get_child_ids_from_response(child_page_results)
+            child_ids = _get_child_ids_from_response(child_page_results)
             child_ids_all += child_ids
         else:
             print(f"Failed to retrieve page - at pagination {i}:", response.status_code)
